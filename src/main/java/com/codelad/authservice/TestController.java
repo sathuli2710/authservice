b5b98f8c1275804a3a962020e9898c8780c944f2
Sathuli, Abul <abul.a@rakuten.com>
2024-01-03 14:41:15 +0530

    Added Security Filters for spring security

--- src/main/java/com/codelad/authservice/TestController.java
+++ src/main/java/com/codelad/authservice/TestController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/auth/api/")
 public class TestController {
     @GetMapping
     public String test() {
